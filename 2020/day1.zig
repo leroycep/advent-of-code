c593ce4f48342b547934a733672415f345985d14
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-12-01 09:17:18 -0700

    Complete challenge 2 of advent of code

--- 2020/day1.zig
+++ 2020/day1.zig
@@ -10,14 +10,25 @@ pub fn main() !void {
     const challenge1_input = try textLinesToIntList(allocator, CHALLENGE1);
     defer allocator.free(challenge1_input);
 
-    const entries = try findEntriesThatSumTo(allocator, challenge1_input, 2020);
+    const entries = (try findEntriesThatSumTo(allocator, challenge1_input, 2020)) orelse return error.EntriesNotFound;
     const numbers = [_]u32{
         challenge1_input[entries[0]],
         challenge1_input[entries[1]],
     };
 
     std.log.info("Entries {} and {} sum to 2020: {} + {}", .{ entries[0], entries[1], numbers[0], numbers[1] });
-    std.log.info("Multiplied, the give: {} * {} = {}", .{ numbers[0], numbers[1], numbers[0] * numbers[1] });
+    std.log.info("Multiplied, they give: {} * {} = {}", .{ numbers[0], numbers[1], numbers[0] * numbers[1] });
+
+    const entries3 = (try find3EntriesThatSumTo(allocator, challenge1_input, 2020)) orelse return error.EntriesNotFound;
+    const numbers3 = [_]u32{
+        challenge1_input[entries3[0]],
+        challenge1_input[entries3[1]],
+        challenge1_input[entries3[2]],
+    };
+
+    std.log.info("{} + {} + {} = {}", .{ numbers3[0], numbers3[1], numbers3[2], numbers3[0] + numbers3[1] + numbers3[2] });
+    std.log.info("Entries {}, {} and {} sum to 2020", .{ entries3[0], entries3[1], entries3[2] });
+    std.log.info("Multiplied, they give: {} * {} * {} = {}", .{ numbers3[0], numbers3[1], numbers3[2], numbers3[0] * numbers3[1] * numbers3[2] });
 }
 
 fn textLinesToIntList(allocator: *std.mem.Allocator, text: []const u8) ![]u32 {
@@ -33,17 +44,30 @@ fn textLinesToIntList(allocator: *std.mem.Allocator, text: []const u8) ![]u32 {
     return numbers.toOwnedSlice();
 }
 
-fn findEntriesThatSumTo(allocator: *std.mem.Allocator, numbers: []const u32, value: u32) ![2]usize {
+fn findEntriesThatSumTo(allocator: *std.mem.Allocator, numbers: []const u32, value: u32) !?[2]usize {
     var seen = std.AutoHashMap(u32, usize).init(allocator);
     defer seen.deinit();
-    
+
     for (numbers) |num, idx| {
-        const complement = value - num;
+        var complement: u32 = undefined;
+        if (@subWithOverflow(u32, value, num, &complement)) continue;
         if (seen.get(complement)) |complement_idx| {
-            return [2]usize{complement_idx, idx};
+            return [2]usize{ complement_idx, idx };
         }
         try seen.put(num, idx);
     }
-    
-    return error.EntriesNotFound;
+
+    return null;
+}
+
+fn find3EntriesThatSumTo(allocator: *std.mem.Allocator, numbers: []const u32, value: u32) !?[3]usize {
+    for (numbers) |num, idx| {
+        const complement = value - num;
+        const otherTwo = try findEntriesThatSumTo(allocator, numbers, complement);
+        if (otherTwo) |idxs| {
+            return [3]usize{ idxs[0], idxs[1], idx };
+        }
+    }
+
+    return null;
 }
