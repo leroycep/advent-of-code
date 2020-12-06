77ebdfeed5dc6fecb2aeb51c254a25dc28732b6e
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-12-06 11:14:46 -0700

    Complete 2020 day 5 challenge 1

--- 2020/day5.zig
+++ 2020/day5.zig
@@ -1,10 +1,5 @@
 const std = @import("std");
 
-const FIRST_ROW = 0;
-const LAST_ROW = 127;
-const FIRST_COL = 0;
-const LAST_COL = 7;
-
 fn decodePartitionSequence(sequence: []const u8) u10 {
     var seatid: u10 = 0;
     for (sequence[0..7]) |char| {
@@ -33,4 +28,25 @@ test "get correct seat id from binary partition sequence" {
     std.testing.expectEqual(@as(u64, 567), decodePartitionSequence("BFFFBBFRRR"));
     std.testing.expectEqual(@as(u64, 119), decodePartitionSequence("FFFBBBFRRR"));
     std.testing.expectEqual(@as(u64, 820), decodePartitionSequence("BBFFBBFRLL"));
+}
+
+const INPUT = @embedFile("./day5.txt");
+
+pub fn main() !void {
+    const out = std.io.getStdOut().writer();
+
+    var highest_seatid: ?u10 = null;
+
+    var line_iter = std.mem.tokenize(INPUT, "\n\r ");
+    while (line_iter.next()) |line| {
+        const seatid = decodePartitionSequence(line);
+
+        if (highest_seatid) |highest| {
+            highest_seatid = std.math.max(seatid, highest);
+        } else {
+            highest_seatid = seatid;
+        }
+    }
+
+    try out.print("The highest seatid is {}\n", .{highest_seatid});
 }
