ff46478fdf743dc8095330e971c350f8abe51032
LeRoyce Pearson <leroycepearson@geemili.xyz>
2020-12-09 11:15:08 -0700

    Complete 2020 day 8 challenge 2

--- 2020/day8.zig
+++ 2020/day8.zig
@@ -35,20 +35,40 @@ pub fn parseInstructions(allocator: *std.mem.Allocator, text: []const u8) ![]Ins
     return instructions.toOwnedSlice();
 }
 
-pub fn executeUntilLoop(allocator: *std.mem.Allocator, program: []const Instruction) !?i64 {
-    var visited = try allocator.alloc(bool, program.len);
-    defer allocator.free(visited);
-    std.mem.set(bool, visited, false);
+const Result = struct {
+    accumulator: i64,
+    pc: usize,
+    jumpNum: u64,
+    terminated: bool,
+};
 
+pub fn executeUntilLoop(allocator: *std.mem.Allocator, program: []const Instruction, nopsAndJmpsOut: ?*std.ArrayList(usize)) !Result {
+    var visited = try allocator.alloc(u64, program.len);
+    defer allocator.free(visited);
+    std.mem.set(u64, visited, 0);
+
+    var jump_num: u64 = 1;
     var accumulator: i64 = 0;
     var pci: i64 = 0;
     while (pci < program.len) {
         const pc = @intCast(usize, pci);
 
-        if (visited[pc]) {
-            return accumulator;
+        if (visited[pc] != 0) {
+            return Result{
+                .accumulator = accumulator,
+                .pc = pc,
+                .jumpNum = visited[pc],
+                .terminated = false,
+            };
         }
-        visited[pc] = true;
+        visited[pc] = jump_num;
+
+        switch (program[pc].op) {
+            .nop, .jmp => if (nopsAndJmpsOut) |list| {
+                try list.append(pc);
+            },
+            .acc => {},
+        }
 
         var should_increment_pc = true;
         switch (program[pc].op) {
@@ -57,6 +77,8 @@ pub fn executeUntilLoop(allocator: *std.mem.Allocator, program: []const Instruct
                 accumulator += program[pc].data;
             },
             .jmp => {
+                //std.log.debug("Jump {}: {} to {}", .{ jump_num, pc, pci + program[pc].data });
+                jump_num += 1;
                 pci += program[pc].data;
                 should_increment_pc = false;
             },
@@ -67,7 +89,12 @@ pub fn executeUntilLoop(allocator: *std.mem.Allocator, program: []const Instruct
         }
     }
 
-    return null;
+    return Result{
+        .accumulator = accumulator,
+        .pc = @intCast(usize, pci),
+        .jumpNum = jump_num,
+        .terminated = true,
+    };
 }
 
 test "parsing instructions" {
@@ -122,7 +149,31 @@ pub fn main() !void {
     const code = try parseInstructions(allocator, INPUT);
     defer allocator.free(code);
 
-    const accum_value = try executeUntilLoop(std.testing.allocator, code);
+    var nopsAndJmps = std.ArrayList(usize).init(allocator);
+    defer nopsAndJmps.deinit();
+
+    const accum_value = try executeUntilLoop(allocator, code, &nopsAndJmps);
 
     try out.print("Accumulator value was {} when loop was detected\n", .{accum_value});
+
+    std.log.info("Testing changing {} nops and jmps", .{nopsAndJmps.items.len});
+    var modifiedCode = try allocator.alloc(Instruction, code.len);
+    defer allocator.free(modifiedCode);
+    for (nopsAndJmps.items) |nopOrJmp| {
+        std.mem.copy(Instruction, modifiedCode, code);
+        modifiedCode[nopOrJmp].op = switch (code[nopOrJmp].op) {
+            .nop => .jmp,
+            .jmp => .nop,
+            .acc => {
+                std.log.warn("Got a `acc` op to flip at {}! Ignoring", .{nopOrJmp});
+                continue;
+            },
+        };
+
+        const res = try executeUntilLoop(allocator, modifiedCode, null);
+        if (res.terminated) {
+            try out.print("Program terminated with value {}\n", .{res.accumulator});
+            break;
+        }
+    }
 }
