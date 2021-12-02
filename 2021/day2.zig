4c1fed4282737e078d62157dbe7ae3536059ebf6
LeRoyce Pearson <me@leroycepearson.dev>
2021-12-02 05:30:23 -0700

    Complete 2021 day 2 challenge 2

--- 2021/day2.zig
+++ 2021/day2.zig
@@ -9,8 +9,18 @@ const Command = enum {
 };
 
 pub fn main() !void {
-    var depth: i32 = 0;
-    var position: i32 = 0;
+    const out = std.io.getStdOut().writer();
+    try out.print("{}\n", .{try challenge1()});
+    try out.print("{}\n", .{try challenge2()});
+}
+
+const Pos = struct {
+    depth: i32 = 0,
+    position: i32 = 0,
+};
+
+pub fn challenge1() !Pos {
+    var pos = Pos{};
 
     var line_iter = std.mem.tokenize(u8, DATA, "\n\r");
     while (line_iter.next()) |line| {
@@ -23,12 +33,38 @@ pub fn main() !void {
         const number = try std.fmt.parseInt(i32, number_str, 10);
 
         switch (cmd) {
-            .up => depth -= number,
-            .down => depth += number,
-            .forward => position += number,
+            .up => pos.depth -= number,
+            .down => pos.depth += number,
+            .forward => pos.position += number,
         }
     }
 
-    const out = std.io.getStdOut().writer();
-    try out.print("{}, {}\n", .{ position, depth });
+    return pos;
+}
+
+pub fn challenge2() !Pos {
+    var pos = Pos{};
+    var aim: i32 = 0;
+
+    var line_iter = std.mem.tokenize(u8, DATA, "\n\r");
+    while (line_iter.next()) |line| {
+        var word_iter = std.mem.tokenize(u8, line, " ");
+        const cmd_str = word_iter.next() orelse return error.InvalidFormat;
+        const number_str = word_iter.next() orelse return error.InvalidFormat;
+        std.debug.assert(word_iter.next() == null);
+
+        const cmd = std.meta.stringToEnum(Command, cmd_str) orelse return error.UnknownCommand;
+        const number = try std.fmt.parseInt(i32, number_str, 10);
+
+        switch (cmd) {
+            .up => aim -= number,
+            .down => aim += number,
+            .forward => {
+                pos.position += number;
+                pos.depth += aim * number;
+            },
+        }
+    }
+
+    return pos;
 }
