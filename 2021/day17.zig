aca1a7b704bddac64f5c821c767066293236a4a8
LeRoyce Pearson <contact@leroycepearson.dev>
2022-10-31 20:06:13 -0600

    Complete 2021 day 17 challenge 2

--- 2021/day17.zig
+++ 2021/day17.zig
@@ -11,7 +11,7 @@ pub fn main() !void {
 
     const out = std.io.getStdOut().writer();
     try out.print("{}\n", .{try challenge1(arena.allocator(), .{ 48, -189 }, .{ 70, -148 })});
-    // try out.print("{}\n", .{try challenge2(arena.allocator(), DATA)});
+    try out.print("{}\n", .{challenge2(.{ 48, -189 }, .{ 70, -148 })});
 }
 
 const GRAVITY = [2]i64{ 0, -1 };
@@ -88,4 +88,26 @@ test passesThroughArea {
     var res = passesThroughArea(.{ 6, 9 }, .{ 20, -10 }, .{ 30, -5 }) orelse return error.ExpectedSome;
     try std.testing.expectEqual(@as(u32, 20), res.step);
     try std.testing.expectEqual(@as(i64, 45), res.max_y);
+}
+
+pub fn challenge2(min: [2]i64, max: [2]i64) u64 {
+    var number_of_velocities: u64 = 0;
+
+    var y: i64 = -1000;
+    while (y <= 1000) : (y += 1) {
+        var x: i64 = -1000;
+        while (x <= 1000) : (x += 1) {
+            const velocity = .{ x, y };
+            if (passesThroughArea(velocity, min, max)) |result| {
+                _ = result;
+                number_of_velocities += 1;
+            }
+        }
+    }
+
+    return number_of_velocities;
+}
+
+test challenge2 {
+    try std.testing.expectEqual(@as(u64, 112), challenge2(.{ 20, -10 }, .{ 30, -5 }));
 }
