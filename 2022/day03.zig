1d8a489452a6f929588743ddfbcea9823afe7ac7
LeRoyce Pearson <contact@leroycepearson.dev>
2022-12-02 22:28:27 -0700

    Complete 2022 day 03 challenge 2

--- 2022/day03.zig
+++ 2022/day03.zig
@@ -60,11 +60,46 @@ test challenge1 {
 
 pub fn challenge2(allocator: std.mem.Allocator, input: []const u8) !i64 {
     _ = allocator;
-    _ = input;
-    return 0;
+
+    var accumulator: i64 = 0;
+
+    var lines_iterator = std.mem.split(u8, input, "\n");
+    main_loop: while (true) {
+        var i: usize = 0;
+        var contained_in_all: u64 = std.math.maxInt(u64);
+        while (i < 3) : (i += 1) {
+            const rucksack = lines_iterator.next() orelse break :main_loop;
+            if (rucksack.len == 0) break :main_loop;
+
+            var items: u64 = 0;
+            for (rucksack) |item| {
+                items |= (@as(u64, 1) << @intCast(u6, item - 'A'));
+            }
+            contained_in_all &= items;
+        }
+        const item = 'A' + @ctz(contained_in_all);
+        switch (item) {
+            'a'...'z' => {
+                accumulator += item - 'a' + 1;
+            },
+            'A'...'Z' => {
+                accumulator += item - 'A' + 27;
+            },
+            else => unreachable,
+        }
+    }
+    return accumulator;
 }
 
 test challenge2 {
-    // try std.testing.expectEqual(@as(i64, 0), try challenge2(std.testing.allocator, INPUT));
-    return error.SkipZigTest;
+    const INPUT =
+        \\vJrwpWtwJgWrhcsFMMfFFhFp
+        \\jqHRNqRjqzjGDLGLrsFMfFZSrLrFZsSL
+        \\PmmdzqPrVvPwwTWBwg
+        \\wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
+        \\ttgJtRGJQctTZtZT
+        \\CrZsJsPPZsGzwwsLwLmpwMDw
+        \\
+    ;
+    try std.testing.expectEqual(@as(i64, 70), try challenge2(std.testing.allocator, INPUT));
 }
