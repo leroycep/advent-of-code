74797cc8be7c424b9cc1b80c3cf51cbb989df7a3
LeRoyce Pearson <contact@leroycepearson.dev>
2022-12-05 22:13:08 -0700

    Complete day 02 challenge 02

--- 2022/day02.zig
+++ 2022/day02.zig
@@ -56,8 +56,40 @@ test challenge1 {
 
 pub fn challenge2(allocator: std.mem.Allocator, input: []const u8) !i64 {
     _ = allocator;
-    _ = input;
-    return 0;
+
+    var total_score: i64 = 0;
+
+    var lines = std.mem.split(u8, input, "\n");
+    while (lines.next()) |line| {
+        if (line.len == 0) continue;
+        var choices_iterator = std.mem.split(u8, line, " ");
+        const opponent_choice_string = choices_iterator.next().?;
+        const player_outcome_string = choices_iterator.next().?;
+        std.debug.assert(opponent_choice_string.len == 1);
+        std.debug.assert(player_outcome_string.len == 1);
+        std.debug.assert(choices_iterator.next() == null);
+
+        const opponent_choice = opponent_choice_string[0] - 'A';
+        const player_outcome = (player_outcome_string[0] - 'X' + 2) % 3;
+        const player_choice = (opponent_choice + player_outcome) % 3;
+        switch (player_outcome) {
+            0 => total_score += 3,
+            1 => total_score += 6,
+            2 => total_score += 0,
+            else => unreachable,
+        }
+        total_score += player_choice + 1;
+    }
+
+    return total_score;
 }
 
-test challenge2 {}
+test challenge2 {
+    const INPUT =
+        \\A Y
+        \\B X
+        \\C Z
+        \\
+    ;
+    try std.testing.expectEqual(@as(i64, 12), try challenge2(std.testing.allocator, INPUT));
+}
