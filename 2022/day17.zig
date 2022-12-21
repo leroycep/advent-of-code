da02e0791c4d3a5120e3f692262292cb72140d17
LeRoyce Pearson <contact@leroycepearson.dev>
2022-12-20 18:58:21 -0700

    Create colormapped grid

--- 2022/day17.zig
+++ 2022/day17.zig
@@ -8,7 +8,7 @@ const ConstGrid = @import("util").ConstGrid;
 
 const DATA = @embedFile("data/day17.txt");
 
-const PIECES = [_]ConstGrid(u1){
+const PIECES = [_]ConstGrid(u8){
     .{
         .data = &.{ 1, 1, 1, 1 },
         .stride = 4,
@@ -68,7 +68,7 @@ test "challenge 2" {
     try std.testing.expectEqual(@as(u64, 1514285714288), output);
 }
 
-fn collides(map: ConstGrid(u1), piece: ConstGrid(u1), pos: @Vector(2, usize)) bool {
+fn collides(map: ConstGrid(u8), piece: ConstGrid(u8), pos: @Vector(2, usize)) bool {
     if (@reduce(.Or, (pos + piece.size) > map.size)) return true;
     const map_region = map.getRegion(pos, piece.size);
 
@@ -81,7 +81,7 @@ fn collides(map: ConstGrid(u1), piece: ConstGrid(u1), pos: @Vector(2, usize)) bo
         std.debug.assert(map_row.len > 0);
         for (piece_row) |piece_tile, i| {
             const map_tile = map_row[i];
-            if (piece_tile == 1 and map_tile == 1) {
+            if (piece_tile >= 1 and map_tile >= 1) {
                 return true;
             }
             tiles_checked += 1;
@@ -92,7 +92,7 @@ fn collides(map: ConstGrid(u1), piece: ConstGrid(u1), pos: @Vector(2, usize)) bo
 }
 
 const Data = struct {
-    map: Grid(u1),
+    map: Grid(u8),
     piece: usize,
     pos: @Vector(2, usize),
     highest_rock: usize,
@@ -114,7 +114,7 @@ const Data = struct {
                 return error.InvalidFormat;
             }
         }
-        const map = try Grid(u1).alloc(allocator, .{ 7, 10_000 });
+        const map = try Grid(u8).allocWithRowAlign(allocator, .{ 7, 10_000 }, 4);
         map.set(0);
         this.* = .{
             .map = map,
@@ -168,7 +168,7 @@ const Data = struct {
     }
 };
 
-fn renderGrid(vg: nanovg, grid: ConstGrid(u1), offset: @Vector(2, f32)) void {
+fn renderGrid(vg: nanovg, grid: ConstGrid(u8), offset: @Vector(2, f32)) void {
     var row_index: usize = 0;
     var rows = grid.iterateRows();
     while (rows.next()) |row| : (row_index += 1) {
@@ -179,6 +179,90 @@ fn renderGrid(vg: nanovg, grid: ConstGrid(u1), offset: @Vector(2, f32)) void {
         }
     }
 }
+
+const ColormappedGrid = struct {
+    vg: nanovg,
+    colormap: nanovg.Image,
+    grid: ConstGrid(u8),
+    grid_image: nanovg.Image,
+
+    const Options = struct {
+        palette: []const [4]u8 = &DEFAULT_PALETTE,
+    };
+
+    const DEFAULT_PALETTE = generate_palette: {
+        var palette: [256][4]u8 = undefined;
+        palette[0] = .{ 0, 0, 0, 0 };
+        // ROY G BIV
+        palette[1] = .{ 0xFF, 0xFF, 0xFF, 0xFF };
+        palette[2] = .{ 0xFF, 0x00, 0x00, 0xFF };
+        palette[3] = .{ 0xFF, 0xAA, 0x00, 0xFF };
+        palette[4] = .{ 0xFF, 0xFF, 0x00, 0xFF };
+        palette[5] = .{ 0x00, 0xFF, 0x00, 0xFF };
+        palette[6] = .{ 0x00, 0x00, 0xFF, 0xFF };
+        palette[7] = .{ 0x4B, 0x00, 0x82, 0xFF };
+        palette[8] = .{ 0x80, 0x00, 0xFF, 0xFF };
+        break :generate_palette palette;
+    };
+
+    fn init(vg: nanovg, grid: ConstGrid(u8), options: Options) @This() {
+        const colormap = vg.createImageRGBA(@intCast(u32, options.palette.len), 1, .{ .nearest = true }, std.mem.sliceAsBytes(options.palette));
+
+        const grid_image = vg.createImageAlpha(
+            @intCast(u32, grid.stride),
+            @intCast(u32, grid.size[1]),
+            .{ .nearest = true },
+            std.mem.sliceAsBytes(grid.data),
+        );
+
+        return @This(){
+            .vg = vg,
+            .colormap = colormap,
+            .grid = grid,
+            .grid_image = grid_image,
+        };
+    }
+
+    fn updateImage(this: *@This(), grid: ConstGrid(u8)) void {
+        std.debug.assert(grid.stride == this.grid.stride);
+        std.debug.assert(std.mem.eql(usize, &this.grid.size, &grid.size));
+        this.vg.updateImage(this.grid_image, std.mem.sliceAsBytes(grid.data));
+        this.grid = grid;
+    }
+
+    fn deinit(this: *@This()) void {
+        this.vg.deleteImage(this.colormap);
+        this.vg.deleteImage(this.grid_image);
+    }
+
+    pub fn draw(this: @This(), offset: @Vector(2, f32), size: @Vector(2, f32)) void {
+        this.drawRegion(offset, size, .{ 0, 0 }, this.grid.size);
+    }
+
+    pub fn drawRegion(this: @This(), offset: @Vector(2, f32), size: @Vector(2, f32), regionPos: [2]usize, regionSize: [2]usize) void {
+        this.vg.beginPath();
+        this.vg.rect(offset[0], offset[1], size[0], size[1]);
+        const image_size = .{
+            size[0] * @intToFloat(f32, this.grid.stride) / @intToFloat(f32, regionSize[0]),
+            size[1] * @intToFloat(f32, this.grid.size[1]) / @intToFloat(f32, regionSize[1]),
+        };
+        const image_offset = .{
+            size[0] * @intToFloat(f32, regionPos[0]) / @intToFloat(f32, regionSize[0]),
+            size[1] * @intToFloat(f32, regionPos[1]) / @intToFloat(f32, regionSize[1]),
+        };
+        this.vg.fillPaint(this.vg.indexedImagePattern(
+            offset[0] - image_offset[0],
+            offset[1] - image_offset[1],
+            image_size[0],
+            image_size[1],
+            0,
+            this.grid_image,
+            this.colormap,
+            1,
+        ));
+        this.vg.fill();
+    }
+};
 
 pub fn main() !void {
     const ctx = try util.Context.init(.{ .title = "Advent of Code - Day 17" });
@@ -192,15 +276,18 @@ pub fn main() !void {
         app_data.paused = !app_data.paused;
     }
 
-    const window_size = try ctx.window.getSize();
-    const framebuffer_size = try ctx.window.getFramebufferSize();
-    const content_scale = try ctx.window.getContentScale();
-    const pixel_ratio = @max(content_scale.x_scale, content_scale.y_scale);
+    var map_image = ColormappedGrid.init(ctx.vg, app_data.map.asConst(), .{});
+    defer map_image.deinit();
 
     while (!ctx.window.shouldClose()) : (app_data.frame += 1) {
         if (!app_data.paused and app_data.piece < 1_000_000) {
             app_data.update();
         }
+
+        const window_size = try ctx.window.getSize();
+        const framebuffer_size = try ctx.window.getFramebufferSize();
+        const content_scale = try ctx.window.getContentScale();
+        const pixel_ratio = @max(content_scale.x_scale, content_scale.y_scale);
 
         gl.viewport(0, 0, framebuffer_size.width, framebuffer_size.height);
         gl.clearColor(0, 0, 0, 1);
@@ -209,15 +296,13 @@ pub fn main() !void {
         ctx.vg.beginFrame(@intToFloat(f32, window_size.width), @intToFloat(f32, window_size.height), pixel_ratio);
 
         const viewport_height = app_data.highest_rock - 7;
-        const last_row_can_see = std.math.min(app_data.map.size[1] - viewport_height, (window_size.height / 8) + 2);
-        const map_region = app_data.map.getRegion(.{ 0, viewport_height }, .{ app_data.map.size[0], last_row_can_see });
+        // const last_row_can_see = std.math.min(app_data.map.size[1] - viewport_height, (window_size.height / 8) + 2);
+        // const map_region = app_data.map.getRegion(.{ 0, viewport_height }, .{ app_data.map.size[0], last_row_can_see });
 
         ctx.vg.scale(8, 8);
 
-        ctx.vg.beginPath();
-        renderGrid(ctx.vg, map_region.asConst(), .{ 0, 0 });
-        ctx.vg.fillColor(nanovg.rgba(0xFF, 0xFF, 0xFF, 0xFF));
-        ctx.vg.fill();
+        map_image.updateImage(app_data.map.asConst());
+        map_image.drawRegion(.{ 0, 0 }, .{ 7, @intToFloat(f32, app_data.map.size[1] - viewport_height) }, .{ 0, viewport_height }, .{ 7, app_data.map.size[1] - viewport_height });
 
         ctx.vg.beginPath();
         renderGrid(ctx.vg, PIECES[app_data.piece % PIECES.len], .{ @intToFloat(f32, app_data.pos[0]), @intToFloat(f32, app_data.pos[1] - viewport_height) });
